8515a2930d0d4a3530612c7168f8f4d6a67d2ffa
Bryan Hughes <bhughes@appcelerator.com>
2012-07-11 17:20:47 -0700

    [TIMOB-9075] Reference Type cleanup (can't actually test it yet).

--- test/tests/Base/getValue.js
+++ test/tests/Base/getValue.js
@@ -3,31 +3,12 @@ var path = require("path"),
 	assert = require("assert");
 
 module.exports = [{
-		name: "Type is not a reference",
+		name: "TEST SUITE NOT DEFINED",
 		testFunction: function() {
-			return Base.getValue(new Base.TypeBoolean());
+			console.log("IMPLEMENT ENTIRE TEST SUITE");
 		},
 		props: {
-			expectedReturnValue: new Base.TypeBoolean()
-		}
-	},{
-		name: "Reference is unresolvable",
-		testFunction: function() {
-			return Base.getValue(new Base.TypeReference());
-		},
-		props: {
-			expectedException: "ReferenceError"
-		}
-	},{
-		name: "Reference has primitive base",
-		testFunction: function() {
-			var ref = new Base.TypeReference();
-			ref.baseValue = new Base.TypeNumber();
-			debugger;
-			return Base.getValue(ref);
-		},
-		props: {
-			expectedReturnValue: new Base.TypeNumber()
+			expectedReturnValue: true
 		}
 	}
 ];
