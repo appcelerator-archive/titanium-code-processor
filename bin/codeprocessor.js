3c9959e75745d7361e8f29b878c8bcfe34612828
Bryan Hughes <bhughes@appcelerator.com>
2012-09-06 17:59:07 -0700

    [TIMOB-9647] More code cleanup.

--- bin/codeprocessor.js
+++ bin/codeprocessor.js
@@ -10,17 +10,17 @@
 var util = require("util"),
 	path = require("path"),
 	fs = require("fs"),
-	NomNom = require("nomnom"),
+	
+	nomnom = require("nomnom"),
 	winston = require("winston"),
-	xml2js = require("xml2js"),
 	wrench = require("wrench"),
+	
 	CodeProcessor = require("node-code-processor"),
-	Exceptions = require("../lib/Exceptions"),
 
 // ******** CLI Options Parsing ********
 
 // Process the cli args
-	parsedOptions = NomNom
+	parsedOptions = nomnom
 		.option("plugin", {
 			abbr: "p",
 			metavar: "MODULE_NAME",
@@ -55,7 +55,8 @@ var util = require("util"),
 	options = {},
 	projectRoot = ".",
 	plugins,
-	tiappxmlpath;
+	entryPoint,
+	startTime;
 
 // Parse the config options
 if (parsedOptions.config) {
@@ -82,46 +83,22 @@ plugins = parsedOptions.plugin || [];
 
 // Make sure that the project exists
 if (!fs.existsSync(projectRoot)) {
-	throw new Exceptions.InvalidArgumentsError("Error: project root '" + projectRoot + "' does not exist.");
+	throw new Error("Error: project root '" + projectRoot + "' does not exist.");
 }
 	
-// Validate the tiapp.xml
-tiappxmlpath = path.join(projectRoot, "tiapp.xml");
-if (!fs.existsSync(tiappxmlpath)) {
-	throw new Exceptions.InvalidArgumentsError("Error: tiapp.xml file '" + tiappxmlpath + "' does not exist.");
+// Calculate the various directories of interest
+entryPoint = path.join(projectRoot, "Resources", "app.js");
+if (!fs.existsSync(entryPoint)) {
+	throw new Error("Error: Project entry point '" + entryPoint + "' does not exist.");
 }
-	
-// Parse the tiapp.xml file
-winston.log("debug", "Processing tiapp.xml '" + tiappxmlpath + "'");
-(new xml2js.Parser()).parseString(fs.readFileSync(tiappxmlpath).toString(), function (err, result) {
-		
-	// Check if there was an error parsing tiapp.xml
-	if (err) {
-		winston.log("error", "Could not parse '" + tiappxmlpath + "': " + err);
-	} else {
-			
-		// Wrap in a set timeout so that exceptions don't get thrown through the xml parser
-		setTimeout(function () {
-				
-			// Calculate the various directories of interest
-			var codeProcessingDirectory = path.join(projectRoot, "build", "codeprocessing"),
-				entryPoint = "app.js",
-				startTime;
-			
-			entryPoint = path.join(projectRoot, "Resources", entryPoint);
-			if (!fs.existsSync(entryPoint)) {
-				throw new Exceptions.InvalidArgumentsError("Error: Project entry point '" + entryPoint + "' does not exist.");
-			}
-			winston.log("debug", "Processing app main '" + entryPoint + "'");
+winston.log("debug", "Processing app entry point '" + entryPoint + "'");
 
-			// Process the code
-			startTime = (new Date()).getTime();
-			CodeProcessor.process([entryPoint], plugins, winston, options);
+// Process the code
+startTime = (new Date()).getTime();
 			
-			winston.log("info", "Code processing finished successfully in " + ((new Date()).getTime() - startTime) + " ms.");
-			winston.log("info", util.inspect(CodeProcessor.getResults(), false, 4));
+CodeProcessor.process([entryPoint], plugins, winston, options);
 			
-			process.exit(result[0] === "normal" ? 0 : 1);
-		}, 0);
-	}
-});
+winston.log("info", "Code processing finished successfully in " + ((new Date()).getTime() - startTime) + " ms.");
+winston.log("info", util.inspect(CodeProcessor.getResults(), false, 4));
+			
+process.exit(result[0] === "normal" ? 0 : 1);
