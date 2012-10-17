350df3e26fae434f7cffb71b0da23454951d52df
Bryan Hughes <bhughes@appcelerator.com>
2012-10-17 13:34:56 -0700

    [TIMOB-10833] Implemented the new unit test harness.

---
+++ tests/lib/harness.js
@@ -0,0 +1,135 @@
+/**
+ * <p>Copyright (c) 2012 by Appcelerator, Inc. All Rights Reserved.
+ * Please see the LICENSE file for information about licensing.</p>
+ * 
+ * Provides a CLI for the code processor unit tests
+ * @author Bryan Hughes &lt;<a href='mailto:bhughes@appcelerator.com'>bhughes@appcelerator.com</a>&gt;
+ */
+
+var fs = require('fs'),
+	path = require('path'),
+	
+	wrench = require('wrench'),
+	
+	fork = require('child_process').fork;
+
+module.exports.run = function (test262Dir, multiThreaded, chapter) {
+	var includeDir = path.join(test262Dir, 'test', 'harness'),
+		testLib = '',
+		fileList = wrench.readdirSyncRecursive(path.join(test262Dir, 'test', 'suite')),
+		testFileNameRegex,
+		testFileRegex = new RegExp(
+			'^((?:(?:\\s*\\/\\/.*)?\\s*\\n)*)' + // Header
+			'(?:\\/\\*\\*?((?:\\s|\\S)*?)\\*\\/\\s*\\n)' + // Data comment
+			'?((?:\\s|\\S)*)$'), // Test case
+		propertySplitRegex = /\s*\n\s*\*\s*@/,
+		starsRegex = /\*/g,
+		propertyNameRegex = /^(\w+)/,
+		successes = 0,
+		total = 0,
+		startTime = Date.now(),
+		tempDir = path.resolve(path.join(__dirname, '..', 'tmp')),
+		i, len = multiThreaded ? require('os').cpus().length : 1,
+		printFinishedCountdown = len; // Most laptops don't like running at 100%
+	
+	// Create the test lib
+	['cth.js', 'sta.js', 'ed.js', 'testBuiltInObject.js', 'testIntl.js'].forEach(function(file) {
+		testLib += '\n\n/****************************************\n' + 
+			' * ' + file + '\n' + 
+			' ****************************************/\n\n' + 
+			fs.readFileSync(path.join(includeDir, file));
+	});
+	
+	// Parse the chapter
+	if (chapter) {
+		chapter = parseInt(chapter);
+		chapter = (chapter < 10 ? '0' : '') + chapter;
+		if (!fs.existsSync(path.join(test262Dir, 'test', 'suite', 'ch' + chapter))) {
+			console.error('Invalid chapter number "' + chapter + '"');
+			process.exit();
+		}
+		testFileNameRegex = RegExp('^ch' + chapter + '[\\/\\\\].*\\.js$');
+		console.log('\nRunning Chapter ' + parseInt(chapter) + ' unit tests using ' + (len > 1 ? len + ' threads' : '1 thread') + '\n');
+	} else {
+		testFileNameRegex = /^ch[0-9][0-68-9][\/\\].*\.js$/
+		console.log('\nRunning all unit tests using ' + (len > 1 ? len + ' threads' : '1 thread') + '\n');
+	}
+	function processFile() {
+		var file = fileList.shift(),
+			testFileContent,
+			testFilePath,
+			testFile,
+			child,
+			match,
+			header,
+			properties,
+			testProperties,
+			body,
+			propMatch,
+			i, len;
+		if (!file) {
+			printFinishedCountdown--;
+			if (!printFinishedCountdown) {
+				console.log('\nAll tests finished in ' + ((Date.now() - startTime) / 60000).toFixed(1) + ' minutes. ' + 
+					successes + ' out of ' + total + ' tests passed');
+					wrench.rmdirSyncRecursive(tempDir);
+			}
+		} else if (testFileNameRegex.test(file)) {
+			
+			// Read in the test case
+			testFile = fs.readFileSync(path.join(test262Dir, 'test', 'suite', file));
+			
+			// Parse the header
+			match = testFileRegex.exec(testFile);
+			if (match) {
+				header = match[1].trim();
+				properties = match[2].trim();
+				body = match[3];
+				testProperties = {};
+				if (properties) {
+					properties = properties.split(propertySplitRegex);
+					testProperties.commentary = properties[0].replace(starsRegex, '\n').trim();
+					properties.shift();
+					for(i = 0, len = properties.length; i < len; i++) {
+						propMatch = propertyNameRegex.exec(properties[i]);
+						if (!propMatch) {
+							throw new Error('Malformed "@" attribute: ' + properties[i]);
+						}
+						if (propMatch[1] in properties) {
+							throw new Error('Duplicate "@" attribute: ' + propMatch[1]);
+						}
+						testProperties[propMatch[1]] = properties[i].replace(propMatch[1], '').replace(starsRegex, '\n').trim();
+					}
+				}
+				
+				// Write the test file plus headers to a temp file
+				testFileContent = testLib + 
+					'\n\n/****************************************\n' + 
+					' * ' + file + '\n' + 
+					' ****************************************/\n\n' + 
+					body;
+				testFilePath = path.join(tempDir, file);
+				wrench.mkdirSyncRecursive(path.dirname(testFilePath));
+				fs.writeFileSync(testFilePath, testFileContent);
+				
+				child = fork(path.resolve(path.join(__dirname, 'runner')));
+				child.send({ file: testFilePath, properties: testProperties});
+				total++;
+				child.on('message', function(message) {
+					console.log((message.success ? 'PASS: ' : 'FAIL: ') + testFilePath + (!message.success ? '\n   ' + message.errorMessage : ''));
+					if (message.success) {
+						successes++;
+					}
+					setTimeout(processFile, 0);
+				});
+			} else {
+				throw new Error('Could not parse test case ' + file);
+			}			
+		} else {
+			setTimeout(processFile, 0);
+		}
+	}
+	for(i = 0; i < len; i++) {
+		processFile();
+	}
+};
