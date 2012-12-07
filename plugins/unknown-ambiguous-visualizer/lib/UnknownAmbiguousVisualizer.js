76b46acdb207a120b4df1c0b410e0c96bcabb068
Bryan Hughes <bhughes@appcelerator.com>
2012-12-07 14:53:19 -0800

    [TIMOB-11939] Code cleanup

--- plugins/unknown-ambiguous-visualizer/lib/UnknownAmbiguousVisualizer.js
+++ plugins/unknown-ambiguous-visualizer/lib/UnknownAmbiguousVisualizer.js
@@ -2,7 +2,7 @@
  * <p>Copyright (c) 2012 by Appcelerator, Inc. All Rights Reserved.
  * Please see the LICENSE file for information about licensing.</p>
  *
- * @module plugins/RequireFinder
+ * @module plugins/AnalysisCoverageVisualizer
  * @author Bryan Hughes &lt;<a href='mailto:bhughes@appcelerator.com'>bhughes@appcelerator.com</a>&gt;
  */
 
@@ -31,7 +31,7 @@ var path = require('path'),
  * @classdesc Provides a CommonJS compliant require() implementation, based on Titanium Mobile's implementations
  *
  * @constructor
- * @name module:plugins/RequireFinder
+ * @name module:plugins/AnalysisCoverageVisualizer
  */
 module.exports = function () {
 	Runtime.on('processingComplete', function() {
@@ -80,7 +80,7 @@ module.exports = function () {
  * Initializes the plugin
  *
  * @method
- * @name module:plugins/RequireFinder#init
+ * @name module:plugins/AnalysisCoverageVisualizer#init
  */
 module.exports.prototype.init = function init() {};
 
@@ -88,7 +88,7 @@ module.exports.prototype.init = function init() {};
 * Gets the results of the plugin
 *
 * @method
- * @name module:plugins/RequireFinder#getResults
+ * @name module:plugins/AnalysisCoverageVisualizer#getResults
 * @returns {Object} A dictionary with two array properties: <code>resolved</code> and <code>unresolved</code>. The
 *		<code>resolved</code> array contains a list of resolved absolute paths to files that were required. The
 *		<code>unresolved</code> array contains a list of unresolved paths, as passed in to the <code>require()</code>
