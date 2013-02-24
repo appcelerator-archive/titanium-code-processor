ab2947aab578dd49d52e68dc43d2a8618d7fb871
Bryan Hughes <bhughes@appcelerator.com>
2013-02-23 17:39:57 -0800

    [TIMOB-12252] Refactored the units tests to use the new utils and simplified a lot of code

--- tests/lib/master.js
+++ tests/lib/master.js
@@ -6,45 +6,82 @@
  * @author Bryan Hughes &lt;<a href='mailto:bhughes@appcelerator.com'>bhughes@appcelerator.com</a>&gt;
  */
 
-var fs = require('fs'),
-	path = require('path'),
-	existsSync = fs.existsSync || path.existsSync,
-	
-	wrench = require('wrench');
+var path = require('path'),
 
-module.exports.run = function (cluster, options) {
-	var test262Dir = options['test-262-dir'],
-		multiThreaded = options['multi-threaded'],
+	wrench = require('wrench'),
+
+	testutiles = require('./testutils');
+
+module.exports.run = function (cluster) {
+	require('child_process').spawn('node', [path.resolve(path.join('..', '..', 'bin', 'assemblebase'))], {
+		stdio: 'inherit'
+	}).on('exit', function() {
+		cluster.fork();
+		run(cluster, processArgs());
+	});
+};
+
+function printHelp() {
+	console.log(
+		'Usage: test [options]\n\n' +
+		'Options:\n' +
+		'   -m --multi-threaded    Enables multi-threaded testing\n' +
+		'   -c --chapter CHAPTER   The chapter/section/file/whatever to test\n' +
+		'   -h --help              Displays this help information');
+}
+
+function processArgs() {
+	var options = {
+			'multi-threaded': false,
+			chapter: ''
+		},
+		args = process.argv,
+		currentArg;
+
+	args.splice(0, 2); // remove "node tests"
+	console.log();
+	while(args.length) {
+		currentArg = args.shift();
+		switch(currentArg) {
+			case '-m':
+			case '--multi-threaded':
+				options['multi-threaded'] = true;
+				break;
+			case '-c':
+			case '--chapter':
+				options.chapter = args.shift();
+				if (!options.chapter) {
+					console.error('A chapter identifier must be specified with the -c flag');
+				printHelp();
+				process.exit(1);
+				}
+				break;
+			case '-h':
+			case '--help':
+				printHelp();
+				process.exit(1);
+				break;
+			default:
+				console.error('Invalid argument "' + currentArg + '"');
+				printHelp();
+				process.exit(1);
+				break;
+		}
+	}
+	return options;
+}
+
+function run(cluster, options) {
+	var multiThreaded = options['multi-threaded'],
 		chapter = options.chapter,
-		section,
-		subSection,
-		match,
-		chapterRegex = /^([0-9]*)$/,
-		sectionRegex = /^([0-9]*)\/([0-9]*)$/,
-		subSectionRegex = /^([0-9]*)\/([0-9]*)\/([0-9]*)$/,
-		fileList = wrench.readdirSyncRecursive(path.join(test262Dir, 'test', 'suite')),
-		prunedFileList = [],
+		testList = [],
 		numTests,
-		testFileNameRegex,
 		successes = 0,
 		total = 0,
 		startTime = Date.now(),
 		testsFailed = [],
-		tempDir = path.resolve(path.join('/', 'tmp', 'titanium-code-processor')),
-		i, len = multiThreaded ? require('os').cpus().length : 1,
-		printFinishedCountdown = len,
-		chapterPath;
-	
-	function getPrettyTime(diff) {
-		var elapsedTime = new Date(diff),
-			seconds = elapsedTime.getUTCSeconds(),
-			minutes = elapsedTime.getUTCMinutes(),
-			hours = elapsedTime.getUTCHours();
-		hours = hours === 0 ? '' : hours === 1 ? '1 hour ' : hours + ' hours ';
-		minutes = minutes === 0 ? hours ? '0 minutes ' : '' : minutes === 1 ? '1 minute ' : minutes + ' minutes ';
-		seconds = seconds === 1 ? '1 second' : seconds + ' seconds';
-		return hours + minutes + seconds;
-	}
+		i, numThreads = multiThreaded ? require('os').cpus().length : 1,
+		printFinishedCountdown = numThreads;
 
 	function createWorker() {
 		var worker = cluster.fork();
@@ -60,36 +97,37 @@ module.exports.run = function (cluster, options) {
 			} else {
 				testsFailed.push(message.file);
 			}
-			
+
 			console.log((message.success ? 'PASS' : 'FAIL') + ' (' + total + ' of ' + numTests +', ' +
-				getPrettyTime((numTests - total) * (Date.now() - startTime) / total) + ' remaining, ' +
+				testutiles.getPrettyTime((numTests - total) * (Date.now() - startTime) / total) + ' remaining, ' +
 				Math.floor(100 * successes / total) + '% pass rate so far): ' +
 				message.file + (!message.success ? '\n   ' + message.errorMessage : ''));
 
 			setTimeout(function () {
-				worker.destroy();
-				processFile(createWorker());
+				processFile(worker);
 			}, 0);
 		});
 		return worker;
 	}
-	
+
 	function processFile(worker) {
-		var file = prunedFileList.shift();
-			
+		var file = testList.shift();
+
 		if (!file) {
 			printFinishedCountdown--;
 			if (!printFinishedCountdown) {
-				console.log('\nAll tests finished in ' + getPrettyTime(Date.now() - startTime) + '. ' +
+				console.log('\nAll tests finished in ' + testutiles.getPrettyTime(Date.now() - startTime) + '. ' +
 					successes + ' out of ' + total + ' tests (' + Math.floor(100 * successes / total) + '%) passed.\n');
 				if (testsFailed.length) {
 					console.log(testsFailed.length + ' test' + (testsFailed.length === 1 ? '' : 's') + ' failed:\n' +
 						testsFailed.sort().join('\n') + '\n');
 				}
-				wrench.rmdirSyncRecursive(tempDir);
+				if (options['test-node']) {
+					wrench.rmdirSyncRecursive(path.resolve(path.join('/', 'tmp', 'titanium-code-processor')));
+				}
 				process.exit();
 			}
-		} else if (testFileNameRegex.test(file)) {
+		} else {
 			worker.send({
 				type: 'processFile',
 				testSuiteFile: file
@@ -98,9 +136,9 @@ module.exports.run = function (cluster, options) {
 				worker.destroy();
 				total++;
 				testsFailed.push(file);
-				
+
 				console.log('FAIL (' + total + ' of ' + numTests +', ' +
-					getPrettyTime((numTests - total) * (Date.now() - startTime) / total) + ' remaining, ' +
+					testutiles.getPrettyTime((numTests - total) * (Date.now() - startTime) / total) + ' remaining, ' +
 					Math.floor(100 * successes / total) + '% pass rate so far): ' +
 					file + '\n   Execution timeout exceeded');
 
@@ -109,76 +147,16 @@ module.exports.run = function (cluster, options) {
 					processFile(createWorker());
 				}, 0);
 			}, 30000);
-		} else {
-			setTimeout(function () {
-				processFile(worker);
-			}, 0);
 		}
 	}
-	
-	// Parse the chapter, section, and subsection
-	if (chapter) {
-		chapter += '';
-		match = chapter.match(chapterRegex);
-		if (!match) {
-			match = chapter.match(sectionRegex);
-			if (!match) {
-				match = chapter.match(subSectionRegex);
-				if (!match) {
-					console.error('Invalid chapter ' + chapter);
-					process.exit(1);
-				}
-			}
-		}
 
-		chapter = match[1];
-		chapterPath = 'ch' + (chapter < 10 ? '0' : '') + chapter;
-		if (!existsSync(path.join(test262Dir, 'test', 'suite', chapterPath))) {
-			console.error('Invalid chapter "' + chapter + '"\n');
-			process.exit();
-		}
-		
-		if (match[2]) {
-			section = match[2];
-			chapterPath = path.join(chapterPath, chapter + '.' + section);
-			if (!existsSync(path.join(test262Dir, 'test', 'suite', chapterPath))) {
-				console.error('Invalid section "' + section + '"\n');
-				process.exit();
-			}
+	testList = testutiles.getTests(chapter);
+	numTests = testList.length;
 
-		}
-		if (match[3]) {
-			subSection = match[3];
-			chapterPath = path.join(chapterPath, chapter + '.' + section + '.' + subSection);
-			if (!existsSync(path.join(test262Dir, 'test', 'suite', chapterPath))) {
-				console.error('Invalid sub section "' + subSection + '"\n');
-				process.exit();
-			}
-		}
-		testFileNameRegex = '^ch' + (chapter < 10 ? '0' : '') + chapter + '[\\/\\\\]';
-		if (section) {
-			testFileNameRegex += chapter + '\\.' + section + '[\\/\\\\]';
-		}
-		if (subSection) {
-			testFileNameRegex += chapter + '\\.' + section + '\\.' + subSection + '[\\/\\\\]';
-		}
-		testFileNameRegex = RegExp(testFileNameRegex + '.*\\.js$');
-	} else {
-		testFileNameRegex = /^ch[0-9][0-68-9][\/\\].*\.js$/;
-	}
-	
-	// Prune the list of tests
-	for(i = 0; i < fileList.length; i++) {
-		if (testFileNameRegex.test(fileList[i])) {
-			prunedFileList.push(fileList[i]);
-		}
-	}
-	numTests = prunedFileList.length;
-	
 	// Run the tests
-	console.log('\nRunning ' + numTests + ' tests from ' + (options.chapter ? 'Chapter ' + options.chapter.toString().replace(/\//g, '.') : 'all chapters') +
-		' using ' + (len > 1 ? len + ' threads' : '1 thread') + '\n');
-	for(i = 0; i < len; i++) {
+	console.log('Running ' + numTests + ' tests from ' + (options.chapter ? 'Chapter ' + options.chapter.toString().replace(/\//g, '.') : 'all chapters') +
+		' using ' + (numThreads > 1 ? numThreads + ' threads' : '1 thread') + '\n');
+	for(i = 0; i < numThreads; i++) {
 		processFile(createWorker());
 	}
-};
+}
