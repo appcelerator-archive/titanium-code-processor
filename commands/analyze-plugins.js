66e3429c9fab2d3eaa823e66337767977e4424de
Bryan Hughes <bhughes@appcelerator.com>
2013-05-09 12:17:58 -0700

    [TIMOB-13118] Ported options and plugins

---
+++ commands/analyze-plugins.js
@@ -0,0 +1,32 @@
+/**
+ * <p>Copyright (c) 2012 by Appcelerator, Inc. All Rights Reserved.
+ * Please see the LICENSE file for information about licensing.</p>
+ *
+ * CLI command interface for the code processor
+ *
+ * @author Bryan Hughes &lt;<a href='mailto:bhughes@appcelerator.com'>bhughes@appcelerator.com</a>&gt;
+ */
+
+var CodeProcessor = require('..'),
+	__ = require('../lib/i18n')(__dirname).__;
+
+exports.desc = exports.extendedDesc = __('analyses a project using the Titanium Code Processor');
+
+exports.config = function () {
+	var conf = {
+		skipBanner: true
+	};
+	return conf;
+};
+
+exports.run = function (logger, config) {
+	var paths = config.paths && config.paths.codeProcessorPlugins || [];
+	CodeProcessor.queryPlugins(paths, function (err, results) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		} else {
+			console.log(JSON.stringify(results, false, '\t'));
+		}
+	});
+};
