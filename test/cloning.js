86919fd178c01cfc5e873c0369f19db9b57f6db3
Bryan Hughes <bhughes@appcelerator.com>
2013-09-06 16:39:08 -0700

    [TIMOB-15031] Implemented cloning of contexts

--- test/cloning.js
+++ test/cloning.js
@@ -21,6 +21,38 @@ describe('Cloning', function () {
 
 	Base.init();
 
+	function compareLexicalEnvironments(original, cloned) {
+		var p;
+		if (original.outer) {
+			should.exist(cloned.outer, 'Cloned lexical environment should have an outer lexical environment');
+		} else {
+			should.not.exist(cloned.outer, 'Cloned lexical environment should not have an outer lexical environment');
+		}
+		if (original.envRec instanceof Base.DeclarativeEnvironmentRecord) {
+			should.ok(cloned.envRec instanceof Base.DeclarativeEnvironmentRecord, 'Cloned environment record should be a declarative environment record');
+			should.ok(Object.keys(original.envRec._bindings).length === Object.keys(cloned.envRec._bindings).length,
+				'Cloned envrionment record should have the same number of bindings as the original');
+			for (p in original.envRec._bindings) {
+				should.exist(cloned.envRec._bindings[p], 'Cloned envrionment record should have binding "' + p + '"');
+				compareValues(original.envRec._bindings[p], cloned.envRec._bindings[p]);
+			}
+		} else {
+			should.ok(cloned.envRec instanceof Base.ObjectEnvironmentRecord, 'Cloned environment record should be an object environment record');
+			compareValues(original.envRec._bindingObject, cloned.envRec._bindingObject);
+		}
+		if (original.outer) {
+			compareLexicalEnvironments(original.outer, cloned.outer);
+		}
+	}
+
+	function compareContexts(original, cloned) {
+		compareLexicalEnvironments(original.lexicalEnvironment , cloned.lexicalEnvironment);
+		compareLexicalEnvironments(original.variableEnvironment , cloned.variableEnvironment);
+		compareValues(original.thisBinding, cloned.thisBinding);
+		should.strictEqual(original.strict, cloned.strict, 'Cloned contexts should have the same strict mode flag');
+		should.strictEqual(original._ambiguousBlock, cloned._ambiguousBlock, 'Cloned contexts should have the same ambiguous block semaphore values');
+	}
+
 	var comparedValues = [];
 	function compareValues(original, cloned) {
 		if (!original && !cloned) {
@@ -41,12 +73,19 @@ describe('Cloning', function () {
 		should.equal(original.className, cloned.className, 'Cloned values should have the same class name');
 		should.equal(Base.type(original), Base.type(cloned), 'Cloned values should have the same type');
 
-		if (type == 'Boolean' || type == 'Number' || type == 'String') {
+		if (type == 'Boolean' || type == 'String') {
 			should.strictEqual(original.value, cloned.value, 'Cloned primitives should have the same value');
 		}
 		if (type == 'String') {
 			should.ok(Base.strictEquals(original._lookupProperty('length').value,
 				cloned._lookupProperty('length').value), 'Cloned strings should have the same length');
+		}
+		if (type == 'Number') {
+			if (isNaN(original.value)) {
+				should.ok(isNaN(cloned.value), 'Cloned primitives should have the same value');
+			} else {
+				should.strictEqual(original.value, cloned.value, 'Cloned primitives should have the same value');
+			}
 		}
 		if (type == 'Object') {
 			should.strictEqual(original.extensible, cloned.extensible,
@@ -154,14 +193,13 @@ describe('Cloning', function () {
 	it('should clone a regex', function () {
 		var original = new Base.RegExpType('^hi', 'g'),
 			cloner = new Base.Cloner(),
-			cloned;
-		cloned = cloner.cloneValue(original);
+			cloned = cloner.cloneValue(original);
 		compareValues(original, cloned);
 		compareValues(original.get('exec').callFunction(new Base.UndefinedType(), [new Base.StringType('hihihi')]),
 			cloned.get('exec').callFunction(new Base.UndefinedType(), [new Base.StringType('hihihi')]));
 	});
 
-	it('should clone an object', function () {
+	it('should clone an error', function () {
 		var errorConstructor = Base.getGlobalObject().get('RangeError'),
 			original = errorConstructor.construct(new Base.UndefinedType(), [ new Base.StringType('This is a range error') ]),
 			cloner = new Base.Cloner(),
@@ -172,8 +210,22 @@ describe('Cloning', function () {
 	it('should clone an unknown type', function () {
 		var original = new Base.UnknownType(),
 			cloner = new Base.Cloner(),
-			cloned;
-		cloned = cloner.cloneValue(original);
+			cloned = cloner.cloneValue(original);
 		compareValues(original, cloned);
+	});
+
+	it('should clone a module context', function () {
+		var original = Base.createModuleContext(AST.parseString('var x = 10; module.exports.y = 20;'), false, true, false),
+			cloner = new Base.Cloner(),
+			cloned = cloner.cloneContext(original);
+		compareContexts(original, cloned);
+	});
+
+	it('should clone a function context', function () {
+		var functionObject = new Base.FunctionType([], AST.parseString('function foo() { return 42; }'), Base.getGlobalContext().lexicalEnvironment, false),
+			original = Base.createFunctionContext(functionObject, new Base.UndefinedType(), [], Base.getGlobalContext().lexicalEnvironment),
+			cloner = new Base.Cloner(),
+			cloned = cloner.cloneContext(original);
+		compareContexts(original, cloned);
 	});
 });
