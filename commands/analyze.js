007ded720fc85df8639c23eb2410566854a4b0c9
Praveen Innamuri <pinnamuri@appcelerator.com>
2014-03-25 11:38:41 -0700

    TIMOB-15987 Code Processor: Running titanium-code-processor using CLI throws error

--- commands/analyze.js
+++ commands/analyze.js
@@ -427,7 +427,9 @@ function validateCLIParameters(logger, config, cli, callback) {
 
 	// Validate the project information
 	ti.validateProjectDir(logger, cli, cli.argv, 'project-dir');
-	ti.validateTiappXml(logger, cli.tiapp);
+	// Initialize tiapp properties to stay compatible with old SDK versions.
+	cli.tiapp.properties || (cli.tiapp.properties = {});
+	ti.validateTiappXml(logger, config, cli.tiapp);
 
 	// Note: we do custom SDK validation because the validateCorrectSDK method does a lot more than we need
 	sdk = cli.tiapp['sdk-version'] || Object.keys(cli.env.sdks).sort().reverse()[0];
